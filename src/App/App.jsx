14e43b806a56a640324a30afec2aacee906d1a14
khanhhongtranle <khanhhong141049@gmail.com>
2020-10-27 16:19:25 +0700

    first commit

---
+++ src/App/App.jsx
@@ -0,0 +1,41 @@
+import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import Boards from "./Components/Boards/Boards";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+function App() {
+    const classes = useStyles();
+  return (
+      <div>
+          <div className={classes.root}>
+              <AppBar position="static">
+                  <Toolbar>
+                      <Typography variant="h6" className={classes.title}>
+                          My Boards
+                      </Typography>
+                      <Button color="inherit">Logout</Button>
+                  </Toolbar>
+              </AppBar>
+          </div>
+          <Boards />
+      </div>
+  );
+}
+
+export default App;
